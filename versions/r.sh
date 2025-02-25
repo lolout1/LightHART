895d27fd190cb3522d06dcbc891724bc0d14540f
Abheek Pradhan <appradhann@gmail.com>
2025-02-25 04:49:37 -0600

    clean

--- versions/r.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python tt.py \
-  --config ./config/smartfallmm/t3.yaml \
-  --work-dir ./exps/teacher_var_time \
-  --model-saved-name teacher_best \
-  --device 0 \
-  --base-lr 1e-4 \
-  --include-val True
